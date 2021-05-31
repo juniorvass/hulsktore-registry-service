f23037f8762e4a41d3e42bc4a1a68b3f3fe70da7
Junior <angel_96vir@hotmail.com>
2021-05-31 18:05:16 -0500

    first develop commit

---
+++ src/main/java/com/todo1/hulkstore/registry/PrototypeApplication.java
@@ -0,0 +1,15 @@
+package com.todo1.hulkstore.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo1/hulkstore/registry/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo1.hulkstore.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
